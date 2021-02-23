745dbeeee075c4cd46cd9395fdb1f14ea4c52fb4
Ingan121 <lssaa91192@gmail.com>
2021-02-23 12:13:17 +0900

    초기 커밋.

---
+++ DWMSwitch/DWMSwitch.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace DWMSwitch
+{
+    partial class DWMSwitch
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DWMSwitch));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dWMSwitchV010ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.launchCMDAsDWMUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkForUpdatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gitHubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "DWMSwitch";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dWMSwitchV010ToolStripMenuItem,
+            this.launchCMDAsDWMUserToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.checkForUpdatesToolStripMenuItem,
+            this.gitHubToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(240, 158);
+            // 
+            // dWMSwitchV010ToolStripMenuItem
+            // 
+            this.dWMSwitchV010ToolStripMenuItem.Enabled = false;
+            this.dWMSwitchV010ToolStripMenuItem.Name = "dWMSwitchV010ToolStripMenuItem";
+            this.dWMSwitchV010ToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.dWMSwitchV010ToolStripMenuItem.Text = "DWMSwitch v0.1.0 by Ingan121";
+            // 
+            // launchCMDAsDWMUserToolStripMenuItem
+            // 
+            this.launchCMDAsDWMUserToolStripMenuItem.Name = "launchCMDAsDWMUserToolStripMenuItem";
+            this.launchCMDAsDWMUserToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.launchCMDAsDWMUserToolStripMenuItem.Text = "Launch CMD as DWM user";
+            this.launchCMDAsDWMUserToolStripMenuItem.Click += new System.EventHandler(this.launchCMDAsDWMUserToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // checkForUpdatesToolStripMenuItem
+            // 
+            this.checkForUpdatesToolStripMenuItem.Name = "checkForUpdatesToolStripMenuItem";
+            this.checkForUpdatesToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.checkForUpdatesToolStripMenuItem.Text = "Check for updates";
+            this.checkForUpdatesToolStripMenuItem.Click += new System.EventHandler(this.checkForUpdatesToolStripMenuItem_Click);
+            // 
+            // gitHubToolStripMenuItem
+            // 
+            this.gitHubToolStripMenuItem.Name = "gitHubToolStripMenuItem";
+            this.gitHubToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.gitHubToolStripMenuItem.Text = "GitHub";
+            this.gitHubToolStripMenuItem.Click += new System.EventHandler(this.gitHubToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // DWMSwitch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 80);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DWMSwitch";
+            this.Text = "Waiting until Explorer starts...";
+            this.Shown += new System.EventHandler(this.DWMSwitch_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dWMSwitchV010ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem launchCMDAsDWMUserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gitHubToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkForUpdatesToolStripMenuItem;
+    }
+}
+
---
+++ DWMSwitch/DWMSwitch.cs
@@ -0,0 +1,275 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DWMSwitch
+{
+    public partial class DWMSwitch : Form
+    {
+        #region Variables
+
+        // Global
+        public string logs;
+        public string[] args = Environment.GetCommandLineArgs();
+        //public RegistryKey dsConfReg = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Ingan121\\DWMSwitch", RegistryKeyPermissionCheck.ReadWriteSubTree);
+        public Version ver = new Version(FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion);
+        public bool donthide = false;
+
+        #endregion
+
+        #region Main
+
+        public DWMSwitch()
+        {
+            InitializeComponent();
+
+            if (args.Any(x => x.Contains("hidetray")))
+            {
+                notifyIcon.Visible = false;
+            }
+
+            if (!args.Any(x => x.Contains("nosock")))
+            {
+                StartSockServer();
+            }
+
+            if (!args.Any(x => x.Contains("noupdchk")))
+            {
+                updateCheck();
+            }
+        }
+
+        #endregion
+
+        private void DWMSwitch_Shown(object sender, EventArgs e)
+        {
+            // Wait until Explorer starts to ensure that the tray icon appears
+            while (Process.GetProcessesByName("explorer").Length == 0)
+            {
+                Thread.Sleep(100);
+            }
+
+            EnableWow64FSRedirection(false);
+
+            Process[] processes = Process.GetProcessesByName("dwm.orig");
+            if (processes.Length > 0)
+            {
+                notifyIcon.Icon = Properties.Resources.DWMOn;
+            }
+            else
+            {
+                notifyIcon.Icon = Properties.Resources.DWMOff;
+            }
+
+            notifyIcon.Visible = true;
+            Visible = false;
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                try
+                {
+                    Process[] processes = Process.GetProcessesByName("dwm.orig");
+                    if (processes.Length > 0)
+                    {
+                        processes[0].Kill();
+                        notifyIcon.Icon = Properties.Resources.DWMOff;
+                    }
+                    else
+                    {
+                        Process.Start("dwm.orig.exe");
+                        notifyIcon.Icon = Properties.Resources.DWMOn;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "DWMSwitch Error");
+                }
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("Kernel32.Dll", EntryPoint = "Wow64EnableWow64FsRedirection")]
+        public static extern bool EnableWow64FSRedirection(bool enable);
+
+        private void launchCMDAsDWMUserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd.exe");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(Resources.Strings.ExitConfirmMessage, Resources.Strings.AppName, MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void gitHubToolStripMenuItem_Click(object sender, EventArgs e) => Process.Start("https://github.com/Ingan121/DWMSwitch/releases");
+
+        private void checkForUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            updateCheck(true);
+        }
+
+        public void log(string str, bool alwaysLog = false) // Logs currently not visible
+        {
+            logs += string.Format("\n{0} : " + str, DateTime.Now);
+        }
+
+        private void updateCheck(bool alertLatest = false)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                log("[Checking for updates...]", true);
+                try
+                {
+                    WebClient wc = new WebClient();
+                    string latestVerStr = wc.DownloadString("https://raw.githubusercontent.com/Ingan121/DWMSwitch/master/latest.txt");
+                    //string latestVerStr = wc.DownloadString("http://localhost/latest.txt");
+                    log("[Lastest version found: " + latestVerStr + "]", true);
+
+                    Version latestVer = new Version(latestVerStr);
+                    int compare = ver.CompareTo(latestVer);
+
+                    if (compare < 0)
+                    {
+                        if (MessageBox.Show("New version of DWMSwitch is available. Download it now?", "DWMSwitch", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            Process.Start("https://github.com/Ingan121/BasicThemer2/releases");
+                        }
+                    }
+                    else
+                    {
+                        if (alertLatest)
+                        {
+                            if (compare == 0)
+                            {
+                                MessageBox.Show("You are running the latest version of DWMSwitch.", "DWMSwitch");
+                            }
+                            else
+                            {
+                                MessageBox.Show("You are running a unreleased version of DWMSwitch.", "DWMSwitch");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    log(ex.ToString(), true);
+                    MessageBox.Show("Update check failed!", "DWMSwitch");
+                }
+            });
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Work in Progress");
+            //new SockTest().Show();
+        }
+
+        private void StartSockServer()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                // 포트 9090으로 서버 설정	
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9090);
+                Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // bind하고 서버 Listen 대기	
+                server.Bind(ipep);
+                server.Listen(20);
+                // 콘솔 출력	
+                log("Server Start");
+                // 다중 접속 시작	
+                while (true)
+                {
+                    // ThreadPool로 접속이 되면	
+                    ThreadPool.QueueUserWorkItem((_) =>
+                    {
+                        // 메시지 버퍼	
+                        StringBuilder sb = new StringBuilder();
+                        // 클라이언트 받기	
+                        Socket client = (Socket)_;
+                        // 클라이언트 정보	
+                        IPEndPoint ip = (IPEndPoint)client.RemoteEndPoint;
+                        // 콘솔 출력	
+                        log("Client connected IP address = " + ip.Address + " : " + ip.Port);
+                        // 메시지 전송	
+                        client.Send(Encoding.Unicode.GetBytes("Welcome server!\r\n>\0"), SocketFlags.None);
+                        try
+                        {
+                            // 수신 대기	
+                            while (true)
+                            {
+                                // 서버로 오는 메시지를 받는다.	
+                                byte[] ret = new byte[2];
+                                client.Receive(ret, 2, SocketFlags.None);
+                                // 메시지를 unicode로 변환해서 버퍼에 넣는다.	
+                                sb.Append(Encoding.Unicode.GetString(ret, 0, 2));
+                                // 개행 + \n이면 콘솔 출력한다.	
+                                if (sb.Length >= 2 && sb[sb.Length - 2] == '\r' && sb[sb.Length - 1] == '\n')
+                                {
+                                    // exit면 접속을 끊는다.	
+                                    if (sb.Length >= 4 && sb[sb.Length - 4] == 'e' && sb[sb.Length - 3] == 'x' && sb[sb.Length - 2] == 'i' && sb[sb.Length - 1] == 't')
+                                    {
+                                        break;
+                                    }
+                                    // 버퍼의 메시지를 콘솔에 출력	
+                                    string msg = sb.ToString();
+                                    // echo 메시지 전송	
+                                    //client.Send(Encoding.Unicode.GetBytes("echo - " + msg + ">\0"), SocketFlags.None);
+                                    // 콘솔 출력	
+                                    log(msg);
+
+                                    try
+                                    {
+                                        Process[] processes = Process.GetProcessesByName("dwm.orig");
+                                        if (processes.Length > 0)
+                                        {
+                                            processes[0].Kill();
+                                            notifyIcon.Icon = Properties.Resources.DWMOff;
+                                        }
+                                        else
+                                        {
+                                            Process.Start("C:\\Windows\\Sysnative\\dwm.orig.exe");
+                                            notifyIcon.Icon = Properties.Resources.DWMOn;
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        client.Send(Encoding.Unicode.GetBytes("Error!" + ex.ToString() + "\r\n>\0"), SocketFlags.None);
+                                        MessageBox.Show(ex.ToString());
+                                    }
+
+                                    // 버퍼를 비운다.	
+                                    sb.Clear();
+                                }
+                            }
+                        }
+                        catch
+                        {
+                            // 에러 발생하면 종료	
+                        }
+                        // 접속 종료 메시지 콘솔 출력	
+                        log("Client disconnected IP address = " + ip.Address + " : " + ip.Port);
+                        // 클라이언트와 접속이 되면 Thread 생성	
+                    }, server.Accept());
+                }
+                server.Close();
+            });
+        }
+    }
+}
