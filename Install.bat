63af5aef7baaab6863cd044b9f20970687da9788
Ingan121 <lssaa91192@gmail.com>
2021-06-05 01:13:46 +0900

    Add installer & uninstaller

---
+++ Install.bat
@@ -0,0 +1,96 @@
+@echo off
+cd /d "%~dp0Files"
+title DWMSwitch Installer
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrator privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%cd%"
+    cd /d "%~dp0Files"
+:--------------------------------------
+
+echo DWMSwitch 0.1.0 by Ingan121
+echo Licensed under the MIT License
+echo https://github.com/Ingan121/DWMSwitch
+echo.
+echo ----------------------------------------------------------
+echo.
+echo Warning: DWMSwitch is not complete yet and not guaranteed to work properly on all devices. Use at your own risk!
+echo You can also manually install DWMSwitch with the instructions in README.md.
+pause
+
+echo.
+echo Creating directories...
+mkdir C:\Windows\System32\DWM >nul 2>nul
+if exist C:\Windows\System32\DWM (echo   C:\Windows\System32\DWM: Success) else echo   C:\Windows\System32\DWM: Fail
+mkdir "C:\Program Files\Ingan121" >nul 2>nul
+if exist "C:\Program Files\Ingan121" (echo   C:\Program Files\Ingan121: Success) else echo   C:\Program Files\Ingan121: Fail
+mkdir "C:\Program Files\Ingan121\DWMSwitch" >nul 2>nul
+if exist "C:\Program Files\Ingan121\DWMSwitch" (echo   C:\Program Files\Ingan121\DWMSwitch: Success) else echo   C:\Program Files\Ingan121\DWMSwitch: Fail
+
+if not exist C:\Windows\System32\DWM\dwm.exe (
+    echo.
+    <nul set /p =Backing up the original DWM... 
+    copy C:\Windows\System32\dwm.exe C:\Windows\System32\dwm.orig-%random%.exe >nul
+    if not errorlevel 1 (echo Success) else echo Fail
+
+    echo.
+    <nul set /p =Moving the original DWM... 
+    takeown /a /f C:\Windows\System32\dwm.exe >nul
+    icacls C:\Windows\System32\dwm.exe /grant Administrators:F >nul
+    move C:\Windows\System32\dwm.exe C:\Windows\System32\DWM\dwm.exe >nul
+    if not errorlevel 1 (echo Success) else echo Fail
+)
+
+echo.
+echo Copying files...
+copy DWMServer.exe "C:\Program Files\Ingan121\DWMSwitch" /y >nul
+if not errorlevel 1 (echo   DWMServer.exe: Success) else echo   DWMServer.exe: Fail
+
+copy DWMSwitch\DWMSwitch.exe "C:\Program Files\Ingan121\DWMSwitch" /y >nul
+if not errorlevel 1 (echo   DWMSwitch.exe: Success) else echo   DWMSwitch.exe: Fail
+
+copy DWMSwitch\DWMSwitch.exe.config "C:\Program Files\Ingan121\DWMSwitch" /y >nul
+if not errorlevel 1 (echo   DWMSwitch.exe.config: Success) else echo   DWMSwitch.exe.config: Fail
+
+copy DWMCtl.exe C:\Windows\dwmctl.exe /y >nul
+if not errorlevel 1 (echo   dwmctl.exe: Success) else echo   DWMCtl.exe: Fail
+
+copy DWMLoaderStub.exe C:\Windows\System32\dwm.exe /y >nul
+if not errorlevel 1 (echo   DWMLoaderStub: Success) else echo   DWMLoaderStub: Fail
+
+echo.
+choice /m "Do you want to automatically start DWMSwitch GUI on startup?"
+if %errorlevel% == 1 (
+    <nul set /p =Registering autostart... 
+    reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v DWMSwitch /t REG_SZ /d "C:\Program Files\Ingan121\DWMSwitch\DWMSwitch.exe" /f >nul
+    if not errorlevel 1 (echo Success) else echo Fail
+)
+
+echo.
+echo Done!
+echo Please sign out and sign in again (or reboot) to complete the installation.
+echo Press any key to close this window.
+timeout 20 >nul
---
+++ Uninstall.bat
@@ -0,0 +1,73 @@
+@echo off
+cd /d "%~dp0Files"
+title DWMSwitch Uninstaller
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrator privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%cd%"
+    cd /d "%~dp0Files"
+:--------------------------------------
+
+choice /m "Are you sure you want to uninstall DWMSwitch?"
+if errorlevel 2 exit /b
+
+echo.
+echo Killing DWMSwitch processes...
+taskkill -im DWMServer.exe -f >nul 2>nul
+if not errorlevel 1 (echo   DWMServer.exe: Success) else echo   DWMServer.exe: Not running
+
+taskkill -im DWMSwitch.exe -f >nul 2>nul
+if not errorlevel 1 (echo   DWMSwitch.exe: Success) else echo   DWMSwitch.exe: Not running
+
+taskkill -im dwmctl.exe -f >nul 2>nul
+if not errorlevel 1 (echo   dwmctl.exe: Success) else echo   dwmctl.exe: Not running
+
+echo.
+<nul set /p =Restoring the original DWM... 
+move /y C:\Windows\System32\DWM\dwm.exe C:\Windows\System32\dwm.exe >nul
+if not errorlevel 1 (echo Success) else echo Fail
+
+echo.
+echo Removing files...
+rmdir /s /q "C:\Program Files\Ingan121\DWMSwitch" >nul 2>nul
+if not exist "C:\Program Files\Ingan121\DWMSwitch" (echo   C:\Program Files\Ingan121\DWMSwitch: Success) else echo   C:\Program Files\Ingan121\DWMSwitch: Fail
+
+del C:\Windows\dwmctl.exe >nul 2>nul
+if not exist C:\Windows\dwmctl.exe (echo   C:\Windows\dwmctl.exe: Success) else echo   C:\Windows\dwmctl.exe: Fail
+
+rmdir C:\Windows\System32\DWM\ >nul 2>nul
+if not exist C:\Windows\System32\DWM (echo   C:\Windows\System32\DWM: Success) else echo   C:\Windows\System32\DWM: Fail
+
+echo.
+<nul set /p =Removing autostart... 
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v DWMSwitch /f >nul 2>nul
+if not errorlevel 1 (echo Success) else echo Not registered for autostart
+
+echo.
+echo Done!
+echo Please sign out and sign in again (or reboot) to complete the uninstallation.
+echo Press any key to close this window.
+timeout 20 >nul
